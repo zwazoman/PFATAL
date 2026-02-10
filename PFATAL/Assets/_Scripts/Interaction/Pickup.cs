7e337b5f93cd20a3cbce781d8f1c87f66f1f2c67
dingo <141416442+zwazoman@users.noreply.github.com>
2026-02-10 14:23:34 +0100

    scroll

--- PFATAL/Assets/_Scripts/Interaction/Pickup.cs
+++ PFATAL/Assets/_Scripts/Interaction/Pickup.cs
@@ -8,7 +8,7 @@ public class Pickup : Interactable
     PlayerInteraction _currentPlayerInteraction;
     //Item _currentItem = null;
 
-    public async override void Interact(PlayerInteraction interaction)
+    public override void Interact(PlayerInteraction interaction)
     {
         base.Interact(interaction);
 
@@ -19,15 +19,6 @@ public class Pickup : Interactable
         else
             print("couldn't equip item");
 
-
-        //SpawnNewItemRPC();
-
-        //while (_currentItem == null)
-        //{
-        //    await Awaitable.NextFrameAsync();
-        //}
-
-        //_currentPlayerInteraction.main.playerHands.Equip(_currentItem);
     }
 
     [Rpc(SendTo.Server)]
@@ -35,28 +26,4 @@ public class Pickup : Interactable
     {
         NetworkObject.Despawn();
     }
-
-    //[Rpc(SendTo.Server)]
-    //void SpawnNewItemRPC(RpcParams rpcParams = default)
-    //{
-    //    NetworkObject itemNetwork = NetworkObject.InstantiateAndSpawn(_itemPrefab, NetworkManager, rpcParams.Receive.SenderClientId, true, true, false, transform.position, transform.rotation);
-
-    //    ReceiveItemRPC(itemNetwork.GetNetworkBehaviourAtOrderIndex(0), RpcTarget.Single(rpcParams.Receive.SenderClientId, RpcTargetUse.Temp));
-
-    //    NetworkObject.Despawn();
-    //}
-
-    //[Rpc(SendTo.SpecifiedInParams)]
-    //void ReceiveItemRPC(NetworkBehaviourReference itemNetworkRef, RpcParams rpcParams)
-    //{
-    //    print(rpcParams.Receive.SenderClientId);
-    //    print(itemNetworkRef.TryGet(out Weapon weapon));
-
-    //    if(itemNetworkRef.TryGet(out Item item))
-    //        print(item.gameObject.name);
-
-    //    //_currentPlayerInteraction.main.playerHands.Equip(item);
-    //    _currentItem = item;
-    //}
-
 }

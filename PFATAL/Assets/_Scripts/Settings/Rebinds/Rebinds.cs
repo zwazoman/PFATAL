fb59b33c8f96dbd1f593b25736a4b7531e615615
Romain <romain.braud@e-artsup.net>
2026-03-02 15:24:19 +0100

    Intégration des rebinds et implémentation du input system dans le characterInput (besoin de retravailler sur l'aim)

--- PFATAL/Assets/_Scripts/Settings/Rebinds/Rebinds.cs
+++ PFATAL/Assets/_Scripts/Settings/Rebinds/Rebinds.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 using TMPro;
 using System.Collections.Generic;
+using _scripts.PlayerCharacter;
 
 public class Rebinds : MonoBehaviour
 {
@@ -12,10 +13,13 @@ public class Rebinds : MonoBehaviour
     private InputActionRebindingExtensions.RebindingOperation _rebindingOperation;
 
     private InputAction _moveAction;
-    [SerializeField] private TMP_Text _rebindLabel;
+    private InputAction _action;
+    private TMP_Text _rebindLabel;
 
     private int _bindingIndex;
 
+    [SerializeField] private PlayerCharacter _playerCharacter;
+    [SerializeField] private GameObject _menu;
     
     private void Awake()
     {
@@ -23,22 +27,42 @@ public class Rebinds : MonoBehaviour
         _moveAction = InputReference;
     }
 
+    //needs to be somewhere else.
+    public void OpenClose(InputAction.CallbackContext context)
+    {
+        if (_menu.activeInHierarchy == true)
+        {
+            _menu.SetActive(false);
+            _playerCharacter.SwapActionMapToPlayer();
+        }
+        else
+        {
+            _menu.SetActive(true);
+            _playerCharacter.SwapActionMapToUI();
+        }
+    }
+
+    //To change later for the script in wich it will do every changes for the UI.
+    //Also it's for giving a specific TMP_Text to the buttons in unity.
+    public void ChangeRebindText(TMP_Text text)
+    {
+        _rebindLabel = text;
+        text.text = "...";
+    }
+
     // If you want to rebind a specific movement -> put in the string one of those WITHOUT ANY UPPERCASES (PAS DE MAJUSCULES) : up / down / left / right
     public void RebindMovements(string movementToChange)
     {
         InputActions.FindActionMap("Player").Disable();
-        _rebindLabel.text = "...";
-        //_rebindingOperation = MoveAction.PerformInteractiveRebinding().OnComplete(operation => RebindCompleted()).Start();
         _bindingIndex = InputReference.action.bindings.IndexOf(x => x.isPartOfComposite && x.name == movementToChange);
         _rebindingOperation = InputReference.action.PerformInteractiveRebinding().WithControlsExcluding("Mouse").WithTargetBinding(_bindingIndex).OnMatchWaitForAnother(.2f).OnComplete(operation => RebindCompleted(_moveAction)).Start();
     }
 
-    public void RebindSingleInput(InputAction action)
+    public void RebindSingleInput(InputActionReference action)
     {
+        _action = action;
         InputActions.FindActionMap("Player").Disable();
-        //To change later for the script in wich it will do every changes for the UI.
-        _rebindLabel.text = "...";
-        _rebindingOperation = action.PerformInteractiveRebinding().OnComplete(operation => RebindCompleted(action)).Start();
+        _rebindingOperation = _action.PerformInteractiveRebinding().OnComplete(operation => RebindCompleted(_action)).Start();
     }
 
     private void RebindCompleted(InputAction action)

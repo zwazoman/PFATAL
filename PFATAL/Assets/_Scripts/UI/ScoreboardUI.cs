7f7f158552186e3de4cbf01441233d4ddcdee433
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2026-03-05 11:51:36 +0100

    ScoreboardUI.cs
    Faut tester avec le GameManager, j'ai tester autrement ça marche à voir si j'ai bien mis les valeurs

--- PFATAL/Assets/_Scripts/UI/ScoreboardUI.cs
+++ PFATAL/Assets/_Scripts/UI/ScoreboardUI.cs
@@ -1,30 +1,37 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreboardUI : MonoBehaviour
 {
     [SerializeField] private GameObject playerCardPrefab;
     [SerializeField] private Transform contentParent;
-    [SerializeField] private LeaderBoardData leaderBoardData;
 
-    void Start()
+    [SerializeField] private GameManager gameManager;
+
+    [SerializeField] private GameObject scoreboardPanel;
+
+    private List<PlayerCardUI> playerCards = new List<PlayerCardUI>();
+
+    void LateUpdate()
     {
-        // Simulate adding player cards to the scoreboard
-        for (int i = 0; i < 4; i++)
+        if (scoreboardPanel.activeSelf)
         {
-            AddPlayerCard($"Player {i + 1}", 0, 0, UnityEngine.Random.Range(10, 100));
+            RefreshUI();
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void RefreshUI()
     {
-        
+        foreach (var player in gameManager.LeaderBoard.entries)
+        {
+            for (int i = 0; i < playerCards.Count; i++)
+            {   
+                if (player.ClientID.ToString() == playerCards[i].playerNameText.text)
+                {
+                    playerCards[i].UpdatePlayerInfo(player.Kills, player.Deaths, UnityEngine.Random.Range(10, 100));
+                }
+            }
+        }
     }
 
     public void AddPlayerCard(string playerName = "Player", int kills = 0, int deaths = 0, int ping = 0)
@@ -32,5 +39,13 @@ public class ScoreboardUI : MonoBehaviour
         GameObject newCard = Instantiate(playerCardPrefab, contentParent);
         PlayerCardUI cardUI = newCard.GetComponent<PlayerCardUI>();
         cardUI.SetPlayerInfo(playerName, kills, deaths, ping);
+        playerCards.Add(cardUI);
+    }
+
+
+    // Change la visibilité du Scoreboard lorsque le joueur appuie sur la touche Tab (ptet faire que quand le joueur maintient la touche Tab)
+    public void ShowScoreboard()
+    {
+        scoreboardPanel.SetActive(!scoreboardPanel.activeSelf);
     }
 }

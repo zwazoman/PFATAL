94c4361dc7871f21033f7d98d98fd57b7a1572af
dingo <141416442+zwazoman@users.noreply.github.com>
2026-03-10 13:32:49 +0100

    setup hammer et tomahawk

--- PFATAL/Assets/_Scripts/GameplaySystems/Combat/Items/Weapons/Melee/Hammer.cs
+++ PFATAL/Assets/_Scripts/GameplaySystems/Combat/Items/Weapons/Melee/Hammer.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class Hammer : Item
+public class Hammer : MeleeWeapon
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- PFATAL/Assets/_Scripts/GameplaySystems/Combat/Items/Weapons/Shooting/Tomahawk.cs
+++ PFATAL/Assets/_Scripts/GameplaySystems/Combat/Items/Weapons/Shooting/Tomahawk.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class Tomahawk : MonoBehaviour
+public class Tomahawk : ProjectileWeapon
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

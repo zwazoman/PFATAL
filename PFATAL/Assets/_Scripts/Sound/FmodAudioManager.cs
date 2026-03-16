251f455e1883a62ea01279241596bb3b2c551a9d
dingo <141416442+zwazoman@users.noreply.github.com>
2026-03-16 16:23:14 +0100

    fin setup fmod

---
+++ PFATAL/Assets/_Scripts/Sound/FmodAudioManager.cs
@@ -0,0 +1,46 @@
+using FMOD.Studio;
+using FMODUnity;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FmodAudioManager : NetworkBehaviour
+{
+    public FmodAudioManager instance { get; private set; }
+
+    List<EventInstance> eventInstances = new();
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if(instance != null)
+            Debug.LogError("plusieurs audiomanagers dans la scene");
+        instance = this;
+
+        SceneManager.activeSceneChanged += (_,_) => CleanUp();
+    }
+
+    public void PlayOneShot(EventReference sound, Vector3 pos)
+    {
+        RuntimeManager.PlayOneShot(sound, pos);
+    }
+
+    public EventInstance CreateInstance(EventReference sound)
+    {
+        EventInstance instance = RuntimeManager.CreateInstance(sound);
+        eventInstances.Add(instance);
+
+        return instance;
+    }
+
+    public void CleanUp()
+    {
+        foreach(EventInstance instance in eventInstances)
+        {
+            instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            instance.release();
+        }
+    }
+}

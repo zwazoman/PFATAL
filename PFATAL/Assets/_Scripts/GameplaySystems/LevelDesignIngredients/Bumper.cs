c4e39b6ca7af2da0abda986eba2615cb844146b6
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2026-03-16 15:05:32 +0100

    Ingrédients LD

--- PFATAL/Assets/_Scripts/GameplaySystems/LevelDesignIngredients/Bumper.cs
+++ PFATAL/Assets/_Scripts/GameplaySystems/LevelDesignIngredients/Bumper.cs
@@ -7,7 +7,7 @@ public class Bumper : MonoBehaviour
     {
         if (collider.TryGetComponent(out PlayerPhysics physics))
         {
-            if(physics.enabled) physics.AddImpulse(transform.up*strength);
+            if(physics.enabled) physics.AddImpulse(transform.up*strength); // TODO : faire un produit scalaire
         }
         // else if (collider.TryGetComponent(out Rigidbody rb))
         // {

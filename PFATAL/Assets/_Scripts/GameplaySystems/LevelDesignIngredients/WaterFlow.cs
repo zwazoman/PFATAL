8d6b0f17625856f8dfb92ae298036f171ac3d3c4
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2026-03-17 14:12:23 +0100

    WaterFlow
    pas sur du truc mais bon

--- PFATAL/Assets/_Scripts/GameplaySystems/LevelDesignIngredients/WaterFlow.cs
+++ PFATAL/Assets/_Scripts/GameplaySystems/LevelDesignIngredients/WaterFlow.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class WaterFlow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Range(0, 360)]
+    public float DirectionalWaterLevel = 5f;
+    public float flowSpeed = 5f;
+
+    private Vector3 _flowDirection;
+
     void Start()
     {
-        
+        UpdateFlowDirection();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerStay(Collider collision)
     {
-        
+        if (collision.TryGetComponent(out PlayerPhysics physics))
+        {
+            if (physics.enabled)
+            {
+                physics.AddForce(_flowDirection * flowSpeed);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        UpdateFlowDirection();
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawRay(transform.position, _flowDirection * 10f);
+    }
+
+    private void UpdateFlowDirection()
+    {
+        _flowDirection = Quaternion.AngleAxis(DirectionalWaterLevel, Vector3.up) * Vector3.forward;
     }
 }

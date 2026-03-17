cd1dba75ee4c2c8ade812493887c7cd9251135cc
Romain <romain.braud@e-artsup.net>
2026-03-17 13:47:36 +0100

    Détecteur de double touche identique

--- PFATAL/Assets/_Scripts/GameplaySystems/PlayerCharacter/PlayerCameraBehaviour.cs
+++ PFATAL/Assets/_Scripts/GameplaySystems/PlayerCharacter/PlayerCameraBehaviour.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class PlayerCameraBehaviour : MonoBehaviour
 {
+    public static float BaseFov = 70;
     [Header("sceneReferences")]
     [SerializeField] PlayerCharacter _playerCharacter;
     [SerializeField] private Camera _cam;
@@ -11,7 +12,7 @@ public class PlayerCameraBehaviour : MonoBehaviour
 
     //FOV scaling
     [Header("FOV scaling")]
-    [SerializeField] private Vector2 FOVRange;
+    [SerializeField] private float FOVVelocityOffset = 3;
     [SerializeField] private float _fovSmoothTime;
     [SerializeField] private float _playerVelocityToFovScalingCurveExponent = 3;
     
@@ -73,7 +74,7 @@ public class PlayerCameraBehaviour : MonoBehaviour
     void LateUpdate()
     {
         //fov
-        float targetFOV = Mathf.Lerp(FOVRange.x, FOVRange.y,
+        float targetFOV = Mathf.Lerp( BaseFov, BaseFov + FOVVelocityOffset,
             Mathf.Pow(_playerCharacter.physics.Velocity.magnitude / _playerCharacter.stateMachine.s_Walking._walkSpeed, _playerVelocityToFovScalingCurveExponent));
         _cam.fieldOfView = Mathf.SmoothDamp(_cam.fieldOfView,targetFOV,ref _fovVel,_fovSmoothTime);
         

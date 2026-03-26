b7dcc3a347905d6ffb6cc88eafa79105a120202e
MeguminFireFox <ambrehardy013@gmail.com>
2026-03-26 11:07:03 +0100

    Ajout de debuglogmanager et collect de donné en partie fonctionnel

--- PFATAL/Assets/_Scripts/PlaytestData/DataCollector.cs
+++ PFATAL/Assets/_Scripts/PlaytestData/DataCollector.cs
@@ -1,26 +1,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.Netcode;
 
 public class DataCollector : MonoBehaviour
 {
+    public static DataCollector Instance;
     private Game game;
     private List<GamePlayerSet> gamePlayerSets = new();
     private List<Score> scores = new();
     private List<Death> deaths = new();
-
-    private int gameId = 0; 
+    private int gameId = 0;
     [SerializeField] private DatabaseRequest _databaseRequest;
 
-    /// <summary>
-    /// Fonction pour enregistrer les données d'une partie.
-    /// </summary>
-    public void RecordGame()
+    private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+        GameManager.Instance.EventOnGameStarted += RecordPlayers;
+        GameManager.Instance.EventOnGameEnded += OnGameEnded;
+    }
+
+    void OnDestroy()
+    {
+        if (GameManager.Instance != null) GameManager.Instance.EventOnGameStarted -= RecordPlayers;
+
+        if (GameManager.Instance != null) GameManager.Instance.EventOnGameEnded -= OnGameEnded;
+    }
+
+    private void OnGameEnded(GameRulesBase.GameResult result)
+    {
+        if (!NetworkManager.Singleton.IsServer) return;
+        RecordGame(result.LeaderBoard);
+    }
+
+
+    public void RecordPlayers()
+    {
+        if (!NetworkManager.Singleton.IsServer) return;
+
+        foreach (ulong clientID in NetworkManager.Singleton.ConnectedClientsIds)
+        {
+            Player player = new Player
+            {
+                Id = (int)clientID,
+                Name = "Player_" + clientID
+            };
+
+            StartCoroutine(_databaseRequest.SendPlayer(player));
+        }
+    }
+
+    /// <summary>
+    /// Fonction pour enregistrer les donnÃ©es d'une partie.
+    /// </summary>
+    public void RecordGame(LeaderBoardData leaderBoard)
+    {
+        gamePlayerSets.Clear();
+        scores.Clear();
+
         StartCoroutine(_databaseRequest.LastGameIdCoroutine((id) =>
         {
             gameId = id + 1;
-
             game = new Game
             {
                 Version = int.Parse(Application.version.Replace(".", "")),
@@ -31,21 +82,21 @@ public class DataCollector : MonoBehaviour
                 Duration = GameManager.DEATH_MATCH_GAME_DURATION
             };
 
-            StartCoroutine(_databaseRequest.SendGame(game,(result) =>
+            StartCoroutine(_databaseRequest.SendGame(game, (result) =>
             {
-                RecordPlayerSet();
-                RecordScore();
+                RecordPlayerSet(leaderBoard);
+                RecordScore(leaderBoard);
                 RecordDeath();
             }));
         }));
     }
 
     /// <summary>
-    /// Fonction pour enregistrer les données des joueurs dans une partie.
+    /// Fonction pour enregistrer les donnÃ©es des joueurs dans une partie.
     /// </summary>
-    public void RecordPlayerSet()
+    public void RecordPlayerSet(LeaderBoardData leaderBoard)
     {
-        foreach (var entry in GameManager.Instance.LeaderBoard.entries)
+        foreach (var entry in leaderBoard.entries)
         {
             gamePlayerSets.Add(new GamePlayerSet
             {
@@ -59,11 +110,11 @@ public class DataCollector : MonoBehaviour
     }
 
     /// <summary>
-    /// Fonction pour enregistrer les données de score d'une partie.
+    /// Fonction pour enregistrer les donnÃ©es de score d'une partie.
     /// </summary>
-    public void RecordScore()
+    public void RecordScore(LeaderBoardData leaderBoard)
     {
-        foreach (var entry in GameManager.Instance.LeaderBoard.entries)
+        foreach (var entry in leaderBoard.entries)
         {
             scores.Add(new Score
             {
@@ -77,7 +128,7 @@ public class DataCollector : MonoBehaviour
     }
 
     /// <summary>
-    /// Fonction pour enregistrer les données de mort d'une partie.
+    /// Fonction pour enregistrer les donnÃ©es de mort d'une partie.
     /// </summary>
     public void RecordDeath()
     {

555f30031836eda4753f9a5c50d1d9ce46da3602
dingo <141416442+zwazoman@users.noreply.github.com>
2026-04-08 16:56:51 +0200

    indicateur cooldown tomahawk

--- PFATAL/Assets/_Scripts/UI/Game/Crosshairs/TomahawkCrosshair.cs
+++ PFATAL/Assets/_Scripts/UI/Game/Crosshairs/TomahawkCrosshair.cs
@@ -2,11 +2,13 @@ using DG.Tweening;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TomahawkCrosshair : Crosshair<Tomahawk>
 {
     [Header("Crosshair settings")]
     [SerializeField] DirectionIndicator _indicator;
+    [SerializeField] Slider _dashCooldoawnSlider;
     [SerializeField] List<GameObject> _ammunitions;
 
     GameObject _tomahawkProj;
@@ -65,5 +67,7 @@ public class TomahawkCrosshair : Crosshair<Tomahawk>
         {
             _indicator.gameObject.SetActive(false);
         }
+
+        _dashCooldoawnSlider.value = weapon.currentDashCooldown / weapon.dashCooldown;
     }
 }

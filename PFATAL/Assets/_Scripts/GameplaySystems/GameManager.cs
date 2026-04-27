f1bcc89cadacdb889885804178fe86df64c12633
nathan-exe <nathan.tazi.1@gmail.com>
2026-04-27 15:41:37 +0200

    g shxfdcgjivokhlbjm:nù,;:

--- PFATAL/Assets/_Scripts/GameplaySystems/GameManager.cs
+++ PFATAL/Assets/_Scripts/GameplaySystems/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : NetworkBehaviour
     [SerializeField] private NetworkTimeSyncManager _timeSyncManager;
     
     //todo : scriptable object avec game settings ?
-    public const float DEATH_MATCH_GAME_DURATION = 300;
+    public const float DEATH_MATCH_GAME_DURATION = 60*4;
 
     public static GameMode gameMode = GameMode.DeathMatch;
     

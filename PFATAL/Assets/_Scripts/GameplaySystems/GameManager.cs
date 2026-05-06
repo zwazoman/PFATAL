f3e2727446f4f7e0cabca6268df60078013318f9
MeguminFireFox <ambrehardy013@gmail.com>
2026-05-06 15:31:54 +0200

    Fix et un to do en moins

--- PFATAL/Assets/_Scripts/GameplaySystems/GameManager.cs
+++ PFATAL/Assets/_Scripts/GameplaySystems/GameManager.cs
@@ -15,7 +15,6 @@ public class GameManager : NetworkBehaviour
     public static GameManager Instance { get; private set ; }
     
     //game rules
-    //todo : scriptable object avec game settings ?
     private GameRulesBase _serverGameRules;
     public GameSetting gameSetting;
     
--- PFATAL/Assets/_Scripts/PlaytestData/DataCollector.cs
+++ PFATAL/Assets/_Scripts/PlaytestData/DataCollector.cs
@@ -248,9 +248,9 @@ public class DataCollector : MonoBehaviour
             VictimId = (long)victim,
             KillerId = (long)killer,
             Weapon = weaponID,
-            Distance = distance,
+            Distance = Mathf.Round(distance * 10) * 0.1f,
             IdGame = gameId,
-            Time = time
+            Time = Mathf.RoundToInt(time)
         });
 
         Debug.Log($"[DataCollector] Mort enregistrée : Victime {victim}, Tueur {killer}, Arme {weaponID}, Distance {distance}, Temps {time}");

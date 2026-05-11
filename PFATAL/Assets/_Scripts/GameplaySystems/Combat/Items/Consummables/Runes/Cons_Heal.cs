c7f6ed64dde938ff584f42e45f77ec585b2ea5ba
nathan-exe <nathan.tazi.1@gmail.com>
2026-05-11 14:07:19 +0200

    anim heal

--- PFATAL/Assets/_Scripts/GameplaySystems/Combat/Items/Consummables/Runes/Cons_Heal.cs
+++ PFATAL/Assets/_Scripts/GameplaySystems/Combat/Items/Consummables/Runes/Cons_Heal.cs
@@ -1,18 +1,56 @@
+using GameplaySystems.PlayerCharacter;
 using UnityEngine;
 
 public class Cons_Heal : Consummable
 {
     [SerializeField] private float _heal = 5f;
 
-    public override void StartUsing()
+    private bool _isPlayingBreakAnimation = false;
+    public override void Equip()
     {
-        base.StartUsing();
+        base.Equip();
+        _isPlayingBreakAnimation = false;
+        
+        //link events
+        hand.animatorEventListener.OnGemBroken += BreakGem;
+        hand.animatorEventListener.OnAnimationFinished+= OnAnimationFinished;
+    }
 
+    public override void UnEquip()
+    {
+        base.UnEquip();
+        //unlink events
+        hand.animatorEventListener.OnGemBroken -= BreakGem;
+        hand.animatorEventListener.OnAnimationFinished -= OnAnimationFinished;
+    }
+
+    //appelé par un event de l'animation
+    void OnAnimationFinished()
+    {
+        if (_isPlayingBreakAnimation)
+        {
+            _isPlayingBreakAnimation = false;
+            BreakItem();
+        }
+    }
+
+    //appelé par un event de l'animation
+    void BreakGem()
+    {
         if (playerCharacter.TryGetComponent(out DamageableObject health))
         {
             health.Heal(_heal);
         }
+    }
+    
+    //quand on click
+    public override void StartUsing()
+    {
+        base.StartUsing();
 
-        BreakItem();
+        //lance l'anim de break
+        if(_isPlayingBreakAnimation) return; 
+        _isPlayingBreakAnimation = true;
+        hand.visuals.PlayAnimation(PlayerHandVisuals.AnimationID.gem_break);
     }
 }

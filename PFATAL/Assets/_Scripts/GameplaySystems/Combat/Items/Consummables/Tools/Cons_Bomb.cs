f6a7c2f49f1c1b15dd91dd730f988e15a99b6c2b
dingo <141416442+zwazoman@users.noreply.github.com>
2026-05-12 16:57:34 +0200

    bomb sounds et autre

--- PFATAL/Assets/_Scripts/GameplaySystems/Combat/Items/Consummables/Tools/Cons_Bomb.cs
+++ PFATAL/Assets/_Scripts/GameplaySystems/Combat/Items/Consummables/Tools/Cons_Bomb.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.VFX;
 
@@ -23,6 +24,7 @@ public class Cons_Bomb : Cons_ThrowableBase
     public override void StartUsing()
     {
         base.StartUsing();
+
         _fuseValue = 0;
         _fuseVFX.Play();
     }
--- PFATAL/Assets/_Scripts/Sound/MovingSoundComponent.cs
+++ PFATAL/Assets/_Scripts/Sound/MovingSoundComponent.cs
@@ -3,24 +3,36 @@ using FMODUnity;
 using STOP_MODE = FMOD.Studio.STOP_MODE;
 using UnityEngine;
 
-public class MovingSoundComponent<T> : SoundComponent<T> where T: Component
+public abstract class MovingSoundComponent<T> : SoundComponent<T> where T: Component
 {
     EventInstance _soundInstance;
 
-    protected override void LinkEvents()
-    {
-    }
+    abstract protected override void LinkEvents();
 
-    protected virtual void StartSound(Sounds sound)
+    protected virtual void StartSound(Sounds sound, GameObject go = null, string parameterName = null, float parameterValue = 100)
     {
+        GameObject currentGO = null;
+
+        if (go == null)
+            currentGO = gameObject;
+        else
+            currentGO = go;
+
         _soundInstance = AudioManager.Instance.CreateInstance(sound, true);
-        RuntimeManager.AttachInstanceToGameObject(_soundInstance, gameObject);
+
+        if(parameterName != null && parameterValue !=100)
+            _soundInstance.setParameterByName(parameterName, parameterValue);
+
+        RuntimeManager.AttachInstanceToGameObject(_soundInstance, currentGO);
         _soundInstance.start();
         AudioManager.Instance.Trigger3dSoundPlayed(_soundInstance);
     }
 
     protected virtual void StopSound()
     {
+        if (!_soundInstance.isValid())
+            return;
+
         _soundInstance.stop(STOP_MODE.ALLOWFADEOUT);
         _soundInstance.release();
     }

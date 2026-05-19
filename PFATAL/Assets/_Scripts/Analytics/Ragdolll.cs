a18f5b3bf8f65d38d58caec26bd74178b8990f55
lNoshirol <145337387+lNoshirol@users.noreply.github.com>
2026-05-19 14:18:17 +0200

    ragdoll des oid

--- PFATAL/Assets/_Scripts/Analytics/Ragdolll.cs
+++ PFATAL/Assets/_Scripts/Analytics/Ragdolll.cs
@@ -6,6 +6,8 @@ public class Ragdolll : MonoBehaviour
 {
     public List<GameObject> RagdollParts = new List<GameObject>();
     public GameObject newParent;
+    public List<GameObject> body = new List<GameObject>();
+    public Rigidbody parentRb;
 
     private void Start()
     {
@@ -15,13 +17,26 @@ public class Ragdolll : MonoBehaviour
     [Button("Activate Ragdoll")]
     public void ActiveRagdoll()
     {
+        foreach (GameObject bodyPart in body)
+        {
+            bodyPart.SetActive(true);
+        }
+
         foreach (GameObject part in RagdollParts)
         {
+            part.SetActive(true);
+
+
             part.transform.SetParent(newParent.transform);
             part.AddComponent<Rigidbody>();
+            Rigidbody rb = part.GetComponent<Rigidbody>();
+            rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+
             Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f), 1, Random.Range(-1f, 1f)).normalized/2;
-            part.GetComponent<Rigidbody>().AddForce(randomDirection * 10, ForceMode.Impulse);
-            part.GetComponent<Rigidbody>().AddTorque(randomDirection * 5, ForceMode.Impulse);
+            Vector3 dir = parentRb.linearVelocity.normalized + randomDirection;
+
+            rb.AddForce(randomDirection * 10, ForceMode.Impulse);
+            rb.AddTorque(randomDirection * 5, ForceMode.Impulse);
         }
     }
 }

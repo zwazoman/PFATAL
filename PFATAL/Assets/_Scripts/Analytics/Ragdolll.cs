96d07c0225d0c87b64d58dc52dd0c60764753d54
lNoshirol <145337387+lNoshirol@users.noreply.github.com>
2026-05-20 10:08:33 +0200

    cadavre kaboum

--- PFATAL/Assets/_Scripts/Analytics/Ragdolll.cs
+++ PFATAL/Assets/_Scripts/Analytics/Ragdolll.cs
@@ -1,20 +1,40 @@
+using _scripts.PlayerCharacter;
 using NaughtyAttributes;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ragdolll : MonoBehaviour
 {
-    public List<GameObject> RagdollParts = new List<GameObject>();
-    public GameObject newParent;
-    public List<GameObject> body = new List<GameObject>();
-    public Rigidbody parentRb;
+    [SerializeField] private List<GameObject> RagdollParts = new List<GameObject>();
+    [SerializeField] private GameObject newParent;
+    [SerializeField] private List<GameObject> body = new List<GameObject>();
+    [SerializeField] private Rigidbody parentRb;
+
+    private List<Vector3> initialPositions = new List<Vector3>();
+    private List<Quaternion> initialRotations = new List<Quaternion>();
 
     private void Start()
     {
         //ActiveRagdoll();
+
+        foreach (GameObject part in RagdollParts)
+        {
+            initialPositions.Add(part.transform.localPosition);
+            initialRotations.Add(part.transform.localRotation);
+        }
+
+        parentRb.GetComponent<PlayerCharacterNetworkStateMachineCallback>().OnStateChanged += (oldState, newState) =>
+        {
+            if (newState == PlayerCharacterNetworkStateMachineCallback.PlayerStateEnum.Dead /*HasFlag(PlayerCharacterNetworkStateMachineCallback.PlayerStateEnum.Dead)*/)
+            {
+                ActiveRagdoll();
+            }
+        };
+
+        //parentRb.GetComponent<PlayerCharacter>().inputs.OnRespawnInput
     }
 
-    [Button("Activate Ragdoll")]
+    [Button("Activate Ragdoll", EButtonEnableMode.Playmode)]
     public void ActiveRagdoll()
     {
         foreach (GameObject bodyPart in body)
@@ -27,16 +47,35 @@ public class Ragdolll : MonoBehaviour
             part.SetActive(true);
 
 
-            part.transform.SetParent(newParent.transform);
-            part.AddComponent<Rigidbody>();
-            Rigidbody rb = part.GetComponent<Rigidbody>();
-            rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+            part.TryGetComponent(out Rigidbody rb);
 
             Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f), 1, Random.Range(-1f, 1f)).normalized/2;
             Vector3 dir = parentRb.linearVelocity.normalized + randomDirection;
 
+            rb.isKinematic = false;
             rb.AddForce(randomDirection * 10, ForceMode.Impulse);
             rb.AddTorque(randomDirection * 5, ForceMode.Impulse);
+        } 
+    }
+
+    [Button("Reset Ragdoll", EButtonEnableMode.Playmode)]
+    public void Reset()
+    {
+        /*foreach (GameObject part in RagdollParts)
+        {
+            part.transform.localPosition = initialPositions[RagdollParts.IndexOf(part)];
+            part.transform.localRotation = initialRotations[RagdollParts.IndexOf(part)];
+        }*/
+
+        for (int i = 0; i < RagdollParts.Count; i++)
+        {
+            RagdollParts[i].TryGetComponent(out Rigidbody rb);
+            if (rb != null)
+            {
+                rb.isKinematic = true;
+            }
+            RagdollParts[i].transform.localPosition = initialPositions[i];
+            RagdollParts[i].transform.localRotation = initialRotations[i];
         }
     }
 }

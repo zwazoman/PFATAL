8cd6f655ee9e9def7c5cf0c832686007afe07f83
dingo <141416442+zwazoman@users.noreply.github.com>
2026-05-21 13:17:33 +0200

    le golem regarde

--- PFATAL/Assets/_Scripts/UI/Menu/LobbyGame.cs
+++ PFATAL/Assets/_Scripts/UI/Menu/LobbyGame.cs
@@ -6,24 +6,19 @@ public class LobbyGame : MonoBehaviour
 {
     [SerializeField] Transform _golemHead;
     [SerializeField] Camera _cam;
-    [SerializeField] float _spaceSize = 1;
+    [SerializeField] float _spaceDistance = 1;
 
     private void LateUpdate()
     {
-        //Vector2 screenCenter = new Vector2(Screen.width/2, Screen.height/2);
-
-        //Vector2 screenCenterToMoseDelta = new Vector2(Input.mousePosition.x, Input.mousePosition.y) - screenCenter;
-
-        //print(screenCenterToMoseDelta);
-
-        //Vector3 LookPoint = _cam.transform.localPosition + new Vector3(screenCenterToMoseDelta.x, screenCenterToMoseDelta.y, 0) * _spaceSize;
-
-        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _spaceSize);
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _spaceDistance);
         Vector3 lookPoint = _cam.ScreenToWorldPoint(mousePos);
 
-        _golemHead.LookAt(lookPoint,_golemHead.forward);
-        //_golemHead.rotation = Quaternion.Euler(_golemHead.rotation.x - 90, _golemHead.rotation.y, _golemHead.rotation.z);
-        //_golemHead.rotation = Quaternion.AngleAxis(_golemHead.rotation.eulerAngles.x + 90, Vector3.right);
-        //_golemHead.rotation = Quaternion.LookRotation(_golemHead.forward, -(lookPoint - _golemHead.position));
+        Vector3 dir = (lookPoint - transform.position).normalized;
+
+        // Rotation qui ferait regarder le forward vers la cible
+        Quaternion targetRot = Quaternion.LookRotation(dir);
+
+        // Compensation : le "bas" du modèle devient le forward logique
+        _golemHead.rotation = targetRot * Quaternion.Euler(-90, 0, 0);
     }
 }

3e57582c1d50c09f0cea03e8b5ce9e72c644a7bb
nathan-exe <nathan.tazi.1@gmail.com>
2026-05-28 16:07:44 +0200

    panel scoreboard mieux

--- PFATAL/Assets/_Scripts/GameplaySystems/GameModes/GameRulesDeathMatch.cs
+++ PFATAL/Assets/_Scripts/GameplaySystems/GameModes/GameRulesDeathMatch.cs
@@ -10,6 +10,12 @@ public class GameRulesDeathMatch : GameRulesBase
         _gameDuration = gameDuration;
     }
 
+    int GetPlayerScore(int deaths, int kills)
+    {
+        const float deathPenaltyWeight = .5f;
+        return Mathf.CeilToInt(kills-deaths * ((float)(kills-deaths)/(kills+deaths)*0.5f+0.5f)*deathPenaltyWeight);
+    }
+    
     protected override void StartGame()
     {
         //setup scoring system
@@ -20,13 +26,13 @@ public class GameRulesDeathMatch : GameRulesBase
                 //quand un joueur meurt, on augmente son nombre de morts et donne un kill au joueur l'ayant tué.
                 
                 player.Score.Deaths++;
-                player.Score.Points = player.Score.Kills-player.Score.Deaths;
+                player.Score.Points = GetPlayerScore(player.Score.Deaths,player.Score.Kills);
                 
                 if (player.Character.health.LastDamageSourceClientID != DamageData.NON_PLAYER_DAMAGE_SOURCE_CLIENT_ID)
                 {
                     PlayerData killer = _players[player.Character.health.LastDamageSourceClientID];
                     killer.Score.Kills++;
-                    killer.Score.Points = killer.Score.Kills-killer.Score.Deaths;
+                    killer.Score.Points = GetPlayerScore(player.Score.Deaths,player.Score.Kills);
                 }
 
                 //puis on update le score board

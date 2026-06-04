10b0c8f09abe59ead0601dbb3afcd820366e8d3e
Romain <romain.braud@e-artsup.net>
2026-06-04 14:35:29 +0100

    Fix de ne pas pouvoir intéragir avec les settings dans la partie

--- PFATAL/Assets/_Scripts/Settings/Setting.cs
+++ PFATAL/Assets/_Scripts/Settings/Setting.cs
@@ -7,6 +7,7 @@ public class Setting : MonoBehaviour
     [SerializeField] private SettingsValues _values;
 
     [Header("GameObjects")]
+    [Tooltip("To change the UI with the modification the player did")]
     [SerializeField] private Slider _sliderFOV;
     [SerializeField] private Slider _sliderSensitivity;
     [SerializeField] private TMP_Text _textMoveUp;
@@ -19,6 +20,10 @@ public class Setting : MonoBehaviour
     [SerializeField] private Slider _sliderGamma;
     [SerializeField] private TMP_Dropdown _dropdownFrames;
     [SerializeField] private Toggle _toggleVSYnc;
+
+    [Header("Modification")]
+    [Tooltip("We put every values as the modified values right at the start")]
+    [SerializeField] private float something;
 
     void Start()
     {
